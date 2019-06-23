5cc1a016e1145467f5a6f6140bf667b4f61e57d3
SnowGG <collinrocks117@gmail.com>
2019-06-23 12:29:50 -0600

    Fixed Major bugs and fixed Nav

--- js/universal.js
+++ js/universal.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Transition effect for navbar
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > 10) { 
+      if($(this).scrollTop() > 1) { 
           $('.navbar').addClass('solid');
           $('.navbar-left img').attr('src', 'img/logo.svg');
       } else {
