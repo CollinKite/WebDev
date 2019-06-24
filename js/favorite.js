9ecd88488a7375558262c7ab3f0a11e6fc4f3591
SnowGG <collinrocks117@gmail.com>
2019-06-24 11:46:47 -0600

    Added Future plans, and Fixed Bugs

--- js/favorite.js
+++ js/favorite.js
@@ -2,3 +2,7 @@ console.log("Ah so you've found my Javascript huh");
 $("header").append("<div class='glitch-window'></div>");
 //fill div with clone of real header
 $( "h1.glitched" ).clone().appendTo( ".glitch-window" );
+$('#youtube').each(function() {
+    var link = $(this).html();
+    $(this).contents().wrap('<a href="https://youtube.com"></a>');
+  });
